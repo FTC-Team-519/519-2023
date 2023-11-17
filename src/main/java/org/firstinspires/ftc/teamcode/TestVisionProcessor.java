ece333fdb93164d4b40fd054f57b281f2d04c731
ftc519 <ftc519@gmail.com>
2023-11-16 20:57:08 -0500

    Work on Object Recognition works though might need to fix where it is looking at

--- src/main/java/org/firstinspires/ftc/teamcode/TestVisionProcessor.java
+++ src/main/java/org/firstinspires/ftc/teamcode/TestVisionProcessor.java
@@ -23,21 +23,59 @@ public class TestVisionProcessor implements VisionProcessor {
 
     @Override
     public Object processFrame(Mat frame, long captureTimeNanos) {
-        double[] indexList = frame.get(0, 0);
-        telemetry.addData("What the thing is", indexList[0] + " " + indexList[1] + " " + indexList[2]);
+        int leftSide = 0;
+        int center = 0;
+        int rightSide = 0;
+        double startTime = System.currentTimeMillis();
+
+        for (int row = 0; row < 480; row++) {
+            for (int col = 0; col < 640; col++) {
+                double[] indexList = frame.get(row, col);
+                double red = indexList[0];
+                double green = indexList[1];
+                double blue = indexList[2];
+                if (col < 213) {
+                    if (red > (green + blue)) {
+                        leftSide++;
+                    }
+                }else if (col > 213 && col < 426){
+                    if (red > (green + blue)) {
+                        center++;
+                    }
+                }else if (col > 426){
+                    if (red > (green + blue)) {
+                        rightSide++;
+                    }
+                }
+            }
+        }
+        double endTime = System.currentTimeMillis();
+        if (leftSide > center && leftSide > rightSide) {
+            telemetry.addData("Where the TSE is: ", "Left Side");
+        }else if (center > leftSide && center > rightSide) {
+            telemetry.addData("Where the TSE is: ", "Center");
+        }else if (rightSide > leftSide && rightSide > center) {
+            telemetry.addData("Where the TSE is: ", "Right Side");
+        }else {
+            telemetry.addData("Where the TSE is: ", "Not Found");
+        }
+        telemetry.addData("Times red seen on left: ", leftSide);
+        telemetry.addData("Times red seen in center: ", center);
+        telemetry.addData("Times red seen on right: ", rightSide);
         telemetry.addData("Width: ", frame.width());
         telemetry.addData("Height", frame.height());
+        telemetry.addData("Time Elapsed: ", (endTime - startTime));
 //        telemetry.update();
         return null;
     }
 
     @Override
     public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
-        Paint test = new Paint();
-        test.setColor(Color.RED);
-        canvas.drawLine(100, 100, 100, 200, test);
-        canvas.drawLine(100, 200, 200, 200, test);
-        canvas.drawLine(200, 200, 200, 100, test);
-        canvas.drawLine(200, 100, 100, 100, test); // These lines draw the square need to then have square move based on what it sees
+//        Paint test = new Paint();
+//        test.setColor(Color.RED);
+//        canvas.drawLine(416, 0, 416, 512, test);
+//        canvas.drawLine(832, 0, 832, 512, test);
+//        telemetry.addData("Canvas Height: ", canvas.getHeight());
+//        telemetry.addData("Canvas Width: ", canvas.getWidth());
     }
 }
