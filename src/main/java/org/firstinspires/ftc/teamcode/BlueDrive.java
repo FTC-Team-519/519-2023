a1bb42c1ce6fd62bd137272e1722be5c0e3a3aa7
ftc519 <ftc519@gmail.com>
2023-12-01 21:15:41 -0500

    Stuff

---
+++ src/main/java/org/firstinspires/ftc/teamcode/BlueDrive.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Blue Autonomous Pixel Drop", group="")
+public class BlueDrive extends PixelDriveThing {
+	public BlueDrive() {
+	super(false);
+	}
+}
